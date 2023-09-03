e5f2144e81e84dc740157f24762794b3b447e270
Dominic Ngoetjana <dngoetjana@iqbusiness.net>
2023-09-03 17:55:38 +0200

    Added new controller

---
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by dngoetjana on 2023/09/02.
+ */
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!");
+        return "Hi, folks.";
+    }
+
+}
